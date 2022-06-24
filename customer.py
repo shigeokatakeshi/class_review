95732cf3d1e9baf1cc239250fa7e8017083e935f
shigeokatakeshi <takeshi0713@gmail.com>
2022-06-24 13:57:49 +0900

    c-4 未完成

--- customer.py
+++ customer.py
@@ -29,41 +29,70 @@
 # ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 # ieyasu.entry_fee() # 1200 という値を返す
 
-from pickle import NONE
+# C-4. 単一の顧客情報をCSV形式で取得できる
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
+
+# tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+# ken.info_csv()  # "Tom Ford,57,1500" という値を返す
+
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
 
 
 class Customer:
-    def __init__(self, first_name, family_name, age=NONE):
+    def __init__(self, first_name, family_name, age=None):
         self.first_name = first_name
         self.family_name = family_name
-        self.age = age
+        self._age = age
 
     def full_name(self):
-        return self.first_name + " " + self.family_name
+        print(self.first_name, self.family_name)
+    
+    def age(self):
+        print(self._age)
 
     def entry_fee(self):
-        if 20 > self.age:
-            return 1000
-        if 20 <= self.age < 65:
-            return 1500
-        if 65 <= self.age:
-            return 1200
+        if 20 > self._age:
+            print(1000)
+        if 20 <= self._age < 65:
+            print(1500)
+        if 65 <= self._age:
+            print(1200)
+
+    def info_csv(self):
+        self.full_name(), self.age(), self.entry_fee()
 
 
 ken = Customer(first_name="Ken", family_name="Tanaka")
 tom = Customer(first_name="Tom", family_name="Ford")
 
-print(tom.full_name())
-print(ken.full_name())
+# tom.full_name()  # "Tom Ford" という値を返す
+tom.full_name()
+# ken.full_name()  # "Ken Tanaka" という値を返す
+ken.full_name()
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 
-print(ken.age)
-print(tom.age)
-print(ieyasu.age)
+# ken.age  # 15 という値を返す
+ken.age()
+# tom.age # 57 という値を返す
+tom.age()
+# ieyasu.age # 73 という値を返す
+ieyasu.age()
 
-print(ken.entry_fee())
-print(tom.entry_fee())
-print(ieyasu.entry_fee())
+# ken.entry_fee()  # 1000 という値を返す
+ken.entry_fee()
+# tom.entry_fee() # 1500 という値を返す
+tom.entry_fee()
+# ieyasu.entry_fee() # 1200 という値を返す
+ieyasu.entry_fee()
+
+# ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
+ken.info_csv()
+# tom.info_csv()  # "Tom Ford,57,1500" という値を返す
+tom.info_csv()
+# ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
+ieyasu.info_csv()
